6c1b712e613ec6e4bb0238adabe0af736cd3e68c
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2020-03-16 00:51:22 +0300

    initial

---
+++ bill logger.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtGui as qtg
+
+class MainWindow(qtw.QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #define anything here (widgets etc.)
+
+
+
+        #end of code
+        self.show() #can be called after creating an instance if it is not meant to show right after init
+
+
+    def callback(self):
+        pass
+
+if __name__ == "__main__":
+    app = qtw.QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
